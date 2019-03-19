defd4be7ea46563166dddab82967902e0419f218
sunshine1086 <45737784+sunshine1086@users.noreply.github.com>
2019-03-19 17:31:35 +0530

    math func

---
+++ MathFunctions/src/MathFunctions/MathTest.java
@@ -0,0 +1,20 @@
+package MathFunctions;
+
+public class MathTest {
+	
+	public static void main(String[]args) {
+		
+		System.out.println(Math.random()); //0.1366313314827139
+		
+		System.out.println(Math.random()*10);//3.248012584290122
+		
+		System.out.println(Math.random()*100);//11.02662011889699
+		
+	    //TypeCasting to int
+		System.out.println((int)(Math.random()*100));//46
+		
+		System.out.println((int)(Math.random()*5));// Generates Below 5
+
+}
+	
+}
