7b2176b2971c07e219f568e4e618565beef233ec
sunshine1086 <45737784+sunshine1086@users.noreply.github.com>
2019-03-19 16:19:49 +0530

    acess commit

---
+++ AccessCheck/src/main/java/classFirst/ClassFirst.java
@@ -0,0 +1,18 @@
+package classFirst;
+
+class ClassFirst {
+	
+	  protected static int minimum_bal;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		minimum_bal=300;
+		
+		
+		ClassTwo c2=new ClassTwo();
+		
+		c2.printmin();
+
+	}
+
+}
---
+++ AccessCheck/src/main/java/classFirst/ClassTwo.java
@@ -0,0 +1,12 @@
+package classFirst;
+
+public class ClassTwo extends ClassFirst{
+	
+	
+	public static void printmin() {
+		minimum_bal=minimum_bal+100;
+		System.out.println(minimum_bal);
+	}
+	
+
+}
